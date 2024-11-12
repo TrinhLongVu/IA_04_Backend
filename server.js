1ae267e2b56b06413e8dec053673910698d61c2a
TrinhLongVu <vucongleo@gmail.com>
2024-11-12 21:11:23 +0700

    first commit

--- server.js
+++ server.js
@@ -17,4 +17,8 @@ connectDB();
 app.use(express.json());
 
 app.use('/api', authRoutes);
+app.get('/', (req, res) => {
+  res.send('API is working!');
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
